1322cc59ad6c8048ec269e89dee3e1d78ad6ca87
talha915 <qkhokhar@sparkcognition.com>
2018-07-18 16:36:42 +0500

    cards fixed

--- src/app/shops/shops.component.ts
+++ src/app/shops/shops.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { shop } from '../Data/Shop';
 @Component({
   selector: 'app-shops',
   templateUrl: './shops.component.html',
   styleUrls: ['./shops.component.css']
 })
 export class ShopsComponent implements OnInit {
-
-  constructor() { }
+  public shopData;
+  constructor() { 
+    this.shopData = shop;
+    console.log("Shop", this.shopData);
+  }
 
   ngOnInit() {
   }
