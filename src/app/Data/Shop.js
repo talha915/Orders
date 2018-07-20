5f0aca1b37612384fc3df17002f61b4d2aa43552
talha915 <qkhokhar@sparkcognition.com>
2018-07-20 18:55:23 +0500

    new again

--- src/app/Data/Shop.js
+++ src/app/Data/Shop.js
@@ -2,7 +2,7 @@ export const shop = [
     {
         id: 1,
         title: 'Lowepro Slingshot Edge 250 AW',
-        image: 'https://themes.woocommerce.com/storefront/wp-content/uploads/sites/78/2016/12/lowepro-slingshot-edge-250.jpg?w=450&h=600&crop=1',
+        image: 'http://demothemesky-be87.kxcdn.com/upstore/wp-content/uploads/2017/05/30-350x420.jpg',
         price: '£109.95',
         Description: 'Made for urban environments, the Slingshot Edge 250 AW is designed to keep your powerful and compact DSLR kit within quick reach. And your go-to tablet, smartphone, headphones, wallet and other essentials well organized.',
         SKU: 'LOWEPRO-BAG-250',
@@ -12,7 +12,7 @@ export const shop = [
     {
         id: 2,
         title: 'Lowepro Pro Roller x300 AW',
-        image: 'https://themes.woocommerce.com/storefront/wp-content/uploads/sites/78/2016/12/lowepro-roller-x300.jpg',
+        image: 'http://demothemesky-be87.kxcdn.com/upstore/wp-content/uploads/2017/05/34-350x420.jpg',
         price: '£499.95',
         Description: 'We have taken our award-winning, proven roller to the next level with the Pro Roller x-Series AW. Its patent-pending MaxFit System™ makes it easy to customize and maximize the amount of pro-sized gear you can fit and travel with.',
         SKU: 'LOWEPRO-BAG-X300',
@@ -22,7 +22,7 @@ export const shop = [
     {
         id: 3,
         title: 'Lowepro ProTactic 350 AW',
-        image: 'https://themes.woocommerce.com/storefront/wp-content/uploads/sites/78/2016/12/lowepro-protactic-350.jpg',
+        image: 'http://demothemesky-be87.kxcdn.com/upstore/wp-content/uploads/2017/05/17-350x420.jpg',
         price: '£199.95',
         Description: 'The ProTactic 350 AW delivers high performance to pro photographers who need an urban-inspired pack that delivers on all fronts: accessibility, versatility, comfort and organization.',
         SKU: 'LOWEPRO-BAG-350',
@@ -32,7 +32,7 @@ export const shop = [
     {
         id: 4,
         title: 'Nikon MB-D17 Multi Battery Power Pack',
-        image: 'https://themes.woocommerce.com/storefront/wp-content/uploads/sites/78/2016/12/nikon-accessory-batterygrip.jpg',
+        image: 'http://demothemesky-be87.kxcdn.com/upstore/wp-content/uploads/2017/05/35-350x420.jpg',
         price: '£449.95',
         Description: 'The MB-D17 is a Multi-Power Battery Pack for the D500, increasing the number of shots per charge that you can get. It features a Shutter release button and Fn button for ease in vertical shooting.',
         SKU: 'NIKON-BATTERY-MB-D17',
@@ -42,7 +42,7 @@ export const shop = [
     {
         id: 5,
         title: 'Nikon EN-EL9 Rechargeable Li-ion Battery',
-        image: 'https://themes.woocommerce.com/storefront/wp-content/uploads/sites/78/2016/12/nikon-accessory-battery.jpg',
+        image: 'http://demothemesky-be87.kxcdn.com/upstore/wp-content/uploads/2017/05/22-350x420.jpg',
         price: '£58.00',
         Description: 'N-EL9 Rechargeable Li-ion Battery can be recharged with the MH-23 Quick Charger.These rechargeable batteries provide extended life and consistent power, even in colder conditions.',
         SKU: 'NIKON-BATTERY-EN-EL9',
@@ -52,7 +52,27 @@ export const shop = [
     {
         id: 6,
         title: 'Nikon SB-300 AF Speedlight',
-        image: 'https://themes.woocommerce.com/storefront/wp-content/uploads/sites/78/2016/12/nikon-speedlight-sb300.jpg',
+        image: 'http://demothemesky-be87.kxcdn.com/upstore/wp-content/uploads/2017/05/19-350x420.jpg',
+        price: '£149.95',
+        Description: 'The right lighting can transform an everyday shot into an extraordinary memory. Even if your camera has a pop-up flash, an external Speedlight provides capabilities and control that every shooter can appreciate—so much more than simply illuminating a dimly lit scene.',
+        SKU: 'NIKON-SPEEDLIGHT-SB-300',
+        category: 'Accessories',
+        Tag: 'Camera Flash'
+    },
+    {
+        id: 7,
+        title: 'Nikon SB-300 AF Speedlight',
+        image: 'http://demothemesky-be87.kxcdn.com/upstore/wp-content/uploads/2015/02/52-1-350x420.jpg',
+        price: '£149.95',
+        Description: 'The right lighting can transform an everyday shot into an extraordinary memory. Even if your camera has a pop-up flash, an external Speedlight provides capabilities and control that every shooter can appreciate—so much more than simply illuminating a dimly lit scene.',
+        SKU: 'NIKON-SPEEDLIGHT-SB-300',
+        category: 'Accessories',
+        Tag: 'Camera Flash'
+    },
+    {
+        id: 8,
+        title: 'Nikon SB-300 AF Speedlight',
+        image: 'http://demothemesky-be87.kxcdn.com/upstore/wp-content/uploads/2015/04/49-1-350x420.jpg',
         price: '£149.95',
         Description: 'The right lighting can transform an everyday shot into an extraordinary memory. Even if your camera has a pop-up flash, an external Speedlight provides capabilities and control that every shooter can appreciate—so much more than simply illuminating a dimly lit scene.',
         SKU: 'NIKON-SPEEDLIGHT-SB-300',
